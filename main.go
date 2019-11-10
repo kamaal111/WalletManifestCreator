be897598cbbaf11a426a62875397a348637f9060
Kamaal Farah <kamaal.f1@gmail.com>
2019-11-10 16:27:21 +0100

    Dynamically selecting and hashing files

--- main.go
+++ main.go
@@ -13,11 +13,19 @@ import (
 
 // Manifest represents the structure of the manifest.json file
 type Manifest struct {
-	Icon   string `json:"icon.png"`
-	Icon2x string `json:"icon@2x.png"`
-	Logo   string `json:"logo.png"`
-	Logo2x string `json:"logo@2x.png"`
-	Pass   string `json:"pass.json"`
+	Icon         string `json:"icon.png,omitempty"`
+	Icon2x       string `json:"icon@2x.png,omitempty"`
+	Logo         string `json:"logo.png,omitempty"`
+	Logo2x       string `json:"logo@2x.png,omitempty"`
+	Background   string `json:"background.png,omitempty"`
+	Background2x string `json:"background@2x.png,omitempty"`
+	Footer       string `json:"footer.png,omitempty"`
+	Footer2x     string `json:"footer@2x.png,omitempty"`
+	Strip        string `json:"strip.png,omitempty"`
+	Strip2x      string `json:"strip@2x.png,omitempty"`
+	Thumbnail    string `json:"thumbnail.png,omitempty"`
+	Thumbnail2x  string `json:"thumbnail@2x.png,omitempty"`
+	Pass         string `json:"pass.json,omitempty"`
 }
 
 func hashItem(filepath string) string {
@@ -41,11 +49,41 @@ func hashItem(filepath string) string {
 
 func main() {
 	data := Manifest{
-		Icon:   hashItem("icon.png"),
-		Icon2x: hashItem("icon@2x.png"),
-		Logo:   hashItem("logo.png"),
-		Logo2x: hashItem("logo@2x.png"),
-		Pass:   hashItem("pass.json"),
+		Pass: hashItem("pass.json"),
+	}
+
+	files, filesError := ioutil.ReadDir("./")
+	if filesError != nil {
+		log.Fatal(filesError)
+	}
+
+	for _, file := range files {
+		switch file.Name() {
+		case "icon.png":
+			data.Icon = hashItem(file.Name())
+		case "icon@2x.png":
+			data.Icon2x = hashItem(file.Name())
+		case "background.png":
+			data.Background = hashItem(file.Name())
+		case "background@2x.png":
+			data.Background2x = hashItem(file.Name())
+		case "logo.png":
+			data.Logo = hashItem(file.Name())
+		case "logo@2x.png":
+			data.Logo2x = hashItem(file.Name())
+		case "footer.png":
+			data.Footer = hashItem(file.Name())
+		case "footer@2x.png":
+			data.Footer2x = hashItem(file.Name())
+		case "strip.png":
+			data.Strip = hashItem(file.Name())
+		case "strip@2x.png":
+			data.Strip2x = hashItem(file.Name())
+		case "thumbnail.png":
+			data.Thumbnail = hashItem(file.Name())
+		case "thumbnail@2x.png":
+			data.Thumbnail2x = hashItem(file.Name())
+		}
 	}
 
 	manifest, _ := json.MarshalIndent(data, "", " ")
