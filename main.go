e1160c14b04b7edfc02b32d1c019ec8226d0d996
kamaal111 <kamaal.f1@gmail.com>
2021-07-11 17:20:32 +0200

    Refactoring for release

--- main.go
+++ main.go
@@ -1,20 +1,3 @@
 package main
 
-import (
-	"encoding/json"
-	"fmt"
-	"io/ioutil"
-	"log"
-
-	"github.com/kamaal111/WalletManifestCreator/hasher"
-)
-
-func main() {
-	manifest, manifestError := json.MarshalIndent(hasher.HashFiles(), "", " ")
-	if manifestError != nil {
-		log.Fatal(manifestError.Error())
-	}
-	_ = ioutil.WriteFile("manifest.json", manifest, 0644)
-
-	fmt.Println("Done!")
-}
+func main() {}
